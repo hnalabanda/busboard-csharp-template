f80d24b9f370078f5c36ca9b026caa3fbce97e38
hnalabanda <hemanalabanda@gmail.com>
2020-02-11 15:57:19 +0000

    Added StopPoints and Stop Point Details class

--- BusBoard.ConsoleApp/StopPoints.cs
+++ BusBoard.ConsoleApp/StopPoints.cs
@@ -1,7 +1,14 @@
-﻿namespace BusBoard
+﻿using System.Collections.Generic;
+namespace BusBoard
 {
     public class StopPoints
     {
-        
+        public List<StopPointDetail> stopPoints { get;set; }
+    }
+    
+    public class StopPointDetail
+    {
+        public string naptanId{ get; set; }
+        public string indicator{ get; set; }
     }
 }
